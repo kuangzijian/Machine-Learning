e06993725648fdafb82696cba903431e7837360e
Jack Kuang <Jack.Kuang@ama.ab.ca>
2019-07-15 17:31:31 -0600

    Added computeCost function

--- Linear Regression with multiple variables/Test.py
+++ Linear Regression with multiple variables/Test.py
@@ -1,5 +1,6 @@
 import numpy as np
 from plotData import plotData
+from computeCost import computeCost
 
 A = []
 with open('ex1data1.txt', 'r') as f:
@@ -7,7 +8,22 @@ with open('ex1data1.txt', 'r') as f:
         A.append(list(map(float,line.split(','))))
 A = np.array(A)
 
-a = list(A[:,0])
-b = list(A[:,1])
+x = A[:,0]
+y = A[:,1]
 
-plotData(a,b)
+#plotData(list(x),list(y))
+
+m = len(x)
+x = x.reshape((-1,1))
+y = y.reshape((-1,1))
+#Add a column of ones to x
+x0 = np.ones((m,1))
+X = np.hstack((x0,x))
+theta = np.array([[0],[0]])
+iterations = 1500
+alpha = 0.01
+
+
+J = computeCost(X, y, theta)
+
+print(J)
