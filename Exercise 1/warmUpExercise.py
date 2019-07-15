c3525da548b0a669b8987d92ef6cdfe017e4ca18
Jack Kuang <Jack.Kuang@ama.ab.ca>
2019-07-15 13:24:26 -0600

    Initial commit

---
+++ Exercise 1/warmUpExercise.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+#WARMUPEXERCISE Example function in octave
+#   A = WARMUPEXERCISE() is an example function that returns the 5x5 identity matrix
+
+A = [];
+# ============= YOUR CODE HERE ==============
+#% Instructions: Return the 5x5 identity matrix
+#%               In octave, we return values by defining which variables
+#%               represent the return values (at the top of the file)
+#%               and then set them accordingly.
+
+A = np.eye(5,5);
+
+print(A)
+
+
+
+#% ===========================================
+
