c1985cd06d29e287d01895d79213d7507fcf9bf6
Jack Kuang <Jack.Kuang@ama.ab.ca>
2019-07-17 12:20:13 -0600

    Added normal equation function

--- Linear Regression with multiple variables/ex1optional.py
+++ Linear Regression with multiple variables/ex1optional.py
@@ -1,9 +1,7 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from plotData import plotData
-from computeCost import computeCost
 from gradientDescent import gradientDescent
 from featureNormalize import featureNormalize
+from normalEqn import normalEqn
 
 # ============= 3. Linear regression with multiple variables =============
 A = np.loadtxt(open('ex1data2.txt', 'r'), delimiter=",")
@@ -16,7 +14,7 @@ m = len(y)
 # ============= 3.1 Feature Normalization =============
 X, mu, sigma = featureNormalize(X)
 
-#Add intercept term to X
+# Add intercept term to X
 x0 = np.ones((m, 1))
 X = np.hstack((x0, X))
 
@@ -33,3 +31,39 @@ theta, _ = gradientDescent(X, y, theta, alpha, num_iters)
 
 # Display gradient descent's result
 print('Theta computed from gradient descent:', theta[0], theta[1], theta[2])
+
+# Estimate the price of a 1650 sq-ft, 3 br house
+# ====================== YOUR CODE HERE ======================
+
+test = np.array([1650, 3])
+test = (test - mu) / sigma
+test = np.hstack((np.ones(1), test))
+
+price = test.dot(theta)
+
+# ============================================================
+
+print('Predicted price of a 1650 sq-ft, 3 br house (using gradient descent)', price)
+
+
+# ================ 3.3 Normal Equations ================
+
+A = np.loadtxt(open('ex1data2.txt', 'r'), delimiter=',')
+A = np.array(A)
+
+X = A[:, 0:2]
+y = A[:, 2]
+m = len(y)
+
+# Add intercept term for X
+
+X = np.hstack((np.ones((m,1)), X))
+
+# Calculate the parameters from the normal equation
+
+theta = normalEqn(X, y)
+print('Theta computed from normal equations:', theta[0], theta[1], theta[2])
+
+price = np.array([1, 1650, 3]).dot(theta)
+print('Predicted price of a 1650 sq-ft, 3 br house (using normal equations):', price)
+
--- Linear Regression with multiple variables/normalEqn.py
+++ Linear Regression with multiple variables/normalEqn.py
@@ -1,8 +1,8 @@
+import numpy as np
+
 # NORMALEQN Computes the closed-form solution to linear regression
 #    NORMALEQN(X,y) computes the closed-form solution to linear
 #    regression using the normal equations.
-
-theta = zeros(size(X, 2), 1);
 
 #  ====================== YOUR CODE HERE ======================
 #  Instructions: Complete the code to compute the closed form solution
@@ -10,3 +10,7 @@ theta = zeros(size(X, 2), 1);
 # 
 
 #  ---------------------- Sample Solution ----------------------
+
+def normalEqn(X, y):
+    theta = np.linalg.pinv(X.T.dot(X)).dot(X.T).dot(y)
+    return theta
