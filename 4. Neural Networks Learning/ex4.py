ecdfae9e58fbe69a3ea726956aa1d4e81aff53b0
Jack Kuang <Jack.Kuang@ama.ab.ca>
2019-08-12 10:43:12 -0600

    Update Directory Name

---
+++ 4. Neural Networks Learning/ex4.py
@@ -0,0 +1,11 @@
+import numpy as np
+import scipy.io as sio
+import matplotlib.pyplot as plt
+import scipy.optimize as opt
+
+from displayData import displayData
+#from nn_cost_function import nn_cost_function
+#from sigmoid_gradient import sigmoid_gradient
+#from rand_initialize_weights import rand_initialize_weights
+#from predict import predict
+
